845392ea4bdcb9f8899eab5929d94d7dc41c2a36
mimimiryn <mimimiryn@gmail.com>
2015-06-12 16:01:49 +0900

    はい２

--- Quiz/ResultViewController.swift
+++ Quiz/ResultViewController.swift
@@ -11,7 +11,22 @@ import UIKit
 class ResultViewController: UIViewController {
      var correctAnswer:Int = 0
     
-    //if
+    if(sum == 5){
+        println("全問正解！　動物マスター！")
+    }else if(sum ==3){
+    println("4問正解！　おしい動物マスターまでもう少し！")
+    }else if(sum == 4){
+    println("3問正解! がんばって！")
+    }else if (sum == 2){
+    println("2問正解、、、　やる気あるー？")
+    }else if (sum==1){
+    println("1問正解、、、　やる気あるー？")
+    }else if (sum==0){
+    println("1問正解、、、　出直してこいっ！！！！")
+    }
+
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
